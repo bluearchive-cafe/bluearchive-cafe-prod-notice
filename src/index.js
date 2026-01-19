1eef4a3050618208c4374a7bab06cb90820df109
roundRekt <roundrekt@icloud.com>
2026-01-19 15:51:55 +0800

    还原返回重定向逻辑

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export default {
     async fetch(request, env, ctx) {
-        const pathname = new URL(request.url).pathname;
-        return Response.redirect(`https://prod-notice.bluearchiveyostar.com${pathname}`, 302);
+        return Response.redirect(`https://prod-notice.bluearchiveyostar.com${new URL(request.url).pathname}`, 302);
     },
 };
